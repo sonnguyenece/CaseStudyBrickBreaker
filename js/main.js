ed3d4d82a62fcaa438110d9feab879bb12c0d726
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-08-01 00:07:51 +0700

    refactor Code

--- js/main.js
+++ js/main.js
@@ -25,26 +25,6 @@ let paraIsSave = false;
 let isGameOver = false;
 let timeIsSave = false;
 
-// function init() {
-//     localStorage.display += "resultTime:" + localStorage.timeResult + "\n";
-//     console.log(localStorage.display);
-//     let player = null;
-//     while (player === null || player === "" || player === " ") {
-//         player = prompt("Please enter your nickname. I just want to know how many people play my game:) ");
-//         if (player === "clearAllData") {
-//             resetLocalStorage();
-//             alert("clear!!!");
-//             player = null;
-//         }
-//     }
-//
-//     if (typeof (Storage) !== "undefined") {
-//         localStorage.countPlay = Number(localStorage.countPlay) + 1;
-//         localStorage.display = localStorage.display + "player name :" + player +
-//             " no." + localStorage.countPlay;
-//     }
-// }
-
 function onKeyup(event) {
     switch (event.which) {
         case PARAMETERKEYBOARD.KEYLEFT:
